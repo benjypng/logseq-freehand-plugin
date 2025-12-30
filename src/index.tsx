94aa6e8bb3c289f32d12ca5df0d1b2fb20374bb3
benjypng <47936793+benjypng@users.noreply.github.com>
2025-12-30 10:03:52 +0800

    fix: prettier

--- src/index.tsx
+++ src/index.tsx
@@ -19,6 +19,7 @@ const main = async () => {
       sibling: false,
     })
     await logseq.Editor.setBlockCollapsed(e.uuid, { flag: true })
+    await logseq.Editor.exitEditingMode(false)
   })
 
   logseq.App.onMacroRendererSlotted(
